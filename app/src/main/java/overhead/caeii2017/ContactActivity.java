02c9e1b1843ef3318df4dc53896c48de028551d8
Pablo Becerra <pablo_becerra94@yahoo.com.ar>
2017-01-26 15:16:42 -0300

    Realizada pantalla de contacto

--- app/src/main/java/overhead/caeii2017/ContactActivity.java
+++ app/src/main/java/overhead/caeii2017/ContactActivity.java
@@ -1,13 +1,21 @@
 package overhead.caeii2017;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
+
+    private TextView coordinationTitle;
+    private TextView coordinationMail;
+    private TextView diffusionTitle;
+    private TextView diffusionMail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,15 @@ public class ContactActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle(R.string.contactTitle);
+        coordinationTitle=(TextView)findViewById(R.id.coordinationTItle);
+        coordinationMail=(TextView)findViewById(R.id.coordinationMail);
+        diffusionTitle=(TextView)findViewById(R.id.diffusionTitle);
+        diffusionMail=(TextView)findViewById(R.id.diffusionMail);
+        setFont(coordinationMail);
+        setFont(diffusionMail);
+        setTitleFont(coordinationTitle);
+        setTitleFont(diffusionTitle);
+
         setDisplayHomeAsUp();
     }
 
@@ -24,6 +41,19 @@ public class ContactActivity extends AppCompatActivity {
         if(supportActionBar!=null){
             supportActionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    private void setTitleFont(TextView textView) {
+        //  Typeface font = Typeface.createFromAsset(getAssets(),"fonts/SignPainter-HouseScript.ttf");
+        Typeface font = Typeface.createFromAsset(getAssets(),"fonts/Montserrat-Bold_0.ttf");
+        textView.setTypeface(font);
+        textView.setTextSize(35);
+        textView.setTextColor(Color.RED);
+    }
+
+    public void setFont(TextView textView) {
+        Typeface font = Typeface.createFromAsset(getAssets(),"fonts/monserratregular.ttf");
+        textView.setTypeface(font);
     }
 
     public void startActivities(View view) { startActivity(new Intent(this, AboutUsActivity.class)); }
