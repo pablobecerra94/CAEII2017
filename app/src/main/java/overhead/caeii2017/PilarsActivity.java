15870797e94d2b9964d557ee407f8906af0b01ef
CrimsonBraixen <martin.artime@hotmail.com>
2017-07-18 18:13:05 -0300

    Separación de cada Pilar en una actividad individual

--- app/src/main/java/overhead/caeii2017/PilarsActivity.java
+++ app/src/main/java/overhead/caeii2017/PilarsActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /**
@@ -18,36 +21,28 @@ public class PilarsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String[]items={"Mision 2050","ADN Industrial","Suma cartas a tu baraja"};
+        ListView itemList;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pilars);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setTitle(getText(R.string.pilars_activity));
-        TextView mision2050 = (TextView) findViewById(R.id.mision2050);
-        setTitleFont(mision2050);
-        TextView industrialADN = (TextView) findViewById(R.id.industrialADN);
-        setTitleFont(industrialADN);
-        TextView deck = (TextView) findViewById(R.id.deck);
-        setTitleFont(deck);
-        TextView mision2050Text = (TextView) findViewById(R.id.mision2050Text);
-        setFont(mision2050Text);
-        TextView industrialADNText = (TextView) findViewById(R.id.industrialADNText);
-        setFont(industrialADNText);
-        TextView deckText = (TextView) findViewById(R.id.deckText);
-        setFont(deckText);
+        setTitle(R.string.pilars_activity);
         setDisplayHomeAsUp();
 
+        itemList=(ListView) findViewById(R.id.itemList);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, items);
+        itemList.setAdapter(adapter);
 
-
-
-    }
-
-    private void setTitleFont(TextView textView) {
-        //  Typeface font = Typeface.createFromAsset(getAssets(),"fonts/SignPainter-HouseScript.ttf");
-        Typeface font = Typeface.createFromAsset(getAssets(),"fonts/Montserrat-Bold_0.ttf");
-        textView.setTypeface(font);
-        //textView.setTextSize(35);
-        textView.setTextColor(Color.RED);
+        itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = adapter.getItem(i);
+                Intent intent = new Intent(getApplicationContext(), PilarsItemActivity.class);
+                intent.putExtra("PilarName", selectedItem);
+                startActivity(intent);
+            }
+        });
     }
 
     private void setDisplayHomeAsUp() {
@@ -55,12 +50,5 @@ public class PilarsActivity extends AppCompatActivity {
         if(supportActionBar!=null){
             supportActionBar.setDisplayHomeAsUpEnabled(true);
         }
-    }
-
-    public void startActivities(View view) { startActivity(new Intent(this, AboutUsActivity.class)); }
-
-    public void setFont(TextView textView) {
-        Typeface font = Typeface.createFromAsset(getAssets(),"fonts/monserratregular.ttf");
-        textView.setTypeface(font);
     }
 }
